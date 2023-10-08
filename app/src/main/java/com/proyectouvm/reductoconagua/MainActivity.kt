b14fc3beb49112e51dec558b35232e2f66251ed1
Leidanmty <ld.andrade@hotmail.com>
2023-10-07 19:38:31 -0600

    Add other activity and end mainActivity

--- app/src/main/java/com/proyectouvm/reductoconagua/MainActivity.kt
+++ app/src/main/java/com/proyectouvm/reductoconagua/MainActivity.kt
@@ -1,12 +1,29 @@
 package com.proyectouvm.reductoconagua
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var btnAppConsumos: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        initComponents()
+        btnListeners()
+    }
 
+    private fun initComponents(){
+        btnAppConsumos =findViewById(R.id.btnAppConsumos)
+    }
+
+    private fun navigateConsumos(){
+        val intent = Intent(this, ConsumosActivity::class.java)
+        startActivity(intent)
+    }
+
+    private fun btnListeners(){
+        btnAppConsumos.setOnClickListener { navigateConsumos() }
     }
 }
